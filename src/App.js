f2dc992350a79dca4168d7c6f2a9cc93c778d52f
Suganthi <suganthikrishnavathi@gmail.com>
2017-02-19 23:20:36 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter'
-import {connect} from 'redux';
 
 class App extends Component {
 
